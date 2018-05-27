9a157db71c4a04925f102f0561eb4094dc84a09f
Bob Pony <robertjodoinjr2@gmail.com>
2018-05-27 15:25:49 -0400

    For RPE 0.3 #1 #2
    Credits to jooya0203 for making this possible, also jvyden since i updated the rep.

---
+++ docs/_posts/TEST.md
@@ -0,0 +1,10 @@
+---
+title: The first new post on the new blog
+layout: post
+image: new_comments/comments-square.png
+image-banner: new_comments/comments.png
+author: Bob Pony
+comments: false
+author-link: https://bobpony.org
+description: This is a test, 123.
+---
--- system32/startnet.cmd
+++ system32/startnet.cmd
@@ -1,11 +1,21 @@
 @echo off
+title Loading Raspberry PE
 wpeinit
+echo Welcome to Raspberry PE!
+echo Please note that this is very buggy and isn't made for production use yet, so be prepared for unseen consquences...
+echo Please visit the website https://raspberrype.bobpony.org for details on Raspberry PE.
+echo SYSTEM INFORMATION
 echo PROCESSOR_ARCHITECTURE = %PROCESSOR_ARCHITECTURE%
 echo PROCESSOR_IDENTIFIER = %PROCESSOR_IDENTIFIER%
 echo PROCESSOR_LEVEL = %PROCESSOR_LEVEL%
 echo PROCESSOR_REVISION = %PROCESSOR_REVISION%
 echo NUMBER_OF_PROCESSORS = %NUMBER_OF_PROCESSORS%
-ver
+title Loading USB Driver...
+echo Now loading the USB Driver, please wait...
 drvload dwusb.inf
+title Loading SD Card Driver...
+echo Now loading the SD Card slot driver, please wait...
 drvload bcm2836sdhc.inf
-
+title Welcome to Raspberry PE!
+ver
+echo If you experience any issues with the USB Mouse or Keyboard, please replug it into the USB port again.
